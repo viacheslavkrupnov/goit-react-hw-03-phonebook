4d0de58dd72094f07b59b69554e654aa9a223e7b
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-07-21 17:05:39 +0300

    completed tasks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import "modern-normalize/modern-normalize.css";
-import "./index.css";
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
 
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
